d8c47916868f51d10a58797301d3e3ebb22f5774
Parth1125 <pgera326@gmail.com>
2023-06-12 20:56:16 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <div className="flex">
+      
+      <h1 className="text-3xl font-bold text-red-400 mx-2 underline ">Hello world!</h1>
+      <h1 className="text-3xl font-bold mx-2 underline "> world!</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
