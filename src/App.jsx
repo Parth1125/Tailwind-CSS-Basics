bb79db14516c11d2c64d584f3e5011c5e400a480
Parth1125 <pgera326@gmail.com>
2023-06-13 22:03:02 +0530

    app.jsx changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import "./App.css";
 function App() {
   return (
     <>
-    <div className="flex">
+    <div className="flex flex-row gap-2">
       
       <h1 className="text-3xl font-bold text-red-400 mx-2 underline ">Hello world!</h1>
       <h1 className="text-3xl font-bold mx-2 underline "> world!</h1>
+      <h1 className="text-3xl font-bold mx-2 text-green-100 underline "> This is my world</h1>
     </div>
     </>
   );
